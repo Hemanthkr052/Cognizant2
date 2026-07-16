331cda1dae63a4688f3a796c862833d0a1c56d2c
Hemanth K R <hemanthkr052@gmail.com>
2026-07-16 23:31:58 +0530

    Inital commit: organized exercises

---
+++ hello.java
@@ -0,0 +1,40 @@
+// // // // class Hello {
+// // // //     public static void main(String[] args) 
+// // // //     {
+// // // //         int num1 =3;
+// // // //         int num2 = 5;
+// // // //         int result =num1+num2;
+// // // //         System.out.println(result);
+        
+        
+// // // //     }
+// // // // }
+// // // class Hello {
+// // //     public static void main(String[] args) 
+// // //     {
+// // //         byte marks =6.5;
+// // //         System.out.println(marks);
+// // //     }
+// // // }
+// // class Hello{
+// //     public static void main(String[] args) {
+// //         // byte b =127;
+// //         int a =12;
+// //         byte k =(byte)a;
+// //         float f =5.6f;
+// //         int t = (int)f;
+// //         System.out.println(t);
+// //     }
+// // }
+// class Hello{
+//     public static void main(String[] args) {
+//         byte a=10;
+//         byte b=30;
+//         int result = a*b;
+//         System.out.println(result); 
+//     }
+// }
+
+
+
+
